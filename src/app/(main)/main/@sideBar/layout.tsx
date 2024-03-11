782df7d57bf009c8b45fc89a7a458f4caec6b7be
GyuhaWang <ethanleast@gmail.com>
2024-03-12 02:03:12 +0900

    layout

--- src/app/(main)/main/@sideBar/layout.tsx
+++ src/app/(main)/main/@sideBar/layout.tsx
@@ -1,8 +1,6 @@
 function SideBarLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className=" min-h-screen  w-[28.87rem] float-left  bg-gray-500 ">
-			{children}
-		</div>
+		<div className=" min-h-screen  w-[28.87rem] bg-gray-500 ">{children}</div>
 	);
 }
 export default SideBarLayout;
--- src/app/(main)/main/layout.tsx
+++ src/app/(main)/main/layout.tsx
@@ -10,13 +10,32 @@ function MainLayout({
 	right: React.ReactNode;
 }) {
 	return (
-		<div className="min-h-screen min-w-1295  ">
-			{sideBar}
-			<div className="flex flex-row grow  w-full min-h-screen  bg-black overflow-auto">
-				{left}
-				{right}
+		<div className="min-h-screen w-full  min-w-1295 bg-black overflow-clip ">
+			<div className="max-h-screen w-[396px] bg-gray-50 float-left overflow-x-hidden overflow-y-clip">
+				<div className="w-full h-[100px]">header</div>
+				<div className=" max-h-screen bg-red-50 flex overflow-x-scroll overflow-y-clip">
+					<div className="  grow 1 bg-blue-50 min-w-[300px] overflow-y-scroll ">
+						<div className=" h-[10000px] w-full bg-purple-50  ">hello</div>
+					</div>
+				</div>
+			</div>
+			<div className=" max-h-screen bg-red-50 flex overflow-x-scroll overflow-y-clip">
+				<div className="  grow 1 bg-blue-50 min-w-[300px] overflow-y-scroll ">
+					<div className=" h-[10000px] w-full bg-purple-50  ">hello</div>
+				</div>
+				<div className="grow 1 bg-yellow-50 min-w-[300px] overflow-y-scroll ">
+					<div className=" h-[10000px] w-full bg-purple-50  ">bye</div>
+				</div>
 			</div>
 		</div>
+
+		// <div className="min-h-screen w-full min-w-1295 flex">
+		// 	{sideBar}
+		// 	<div className="grow 1">
+		// 		{left}
+		// 		{right}
+		// 	</div>
+		// </div>
 	);
 }
 export default MainLayout;
